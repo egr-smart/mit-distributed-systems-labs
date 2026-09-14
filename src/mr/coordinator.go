62594a9abac1c700d24d048b5f19143813146771
Ed <esmartqq@gmail.com>
2026-09-14 21:09:24 +1000

    make task status an enum, instantiate task lists in MakeCoordinator

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -18,9 +18,16 @@ type Coordinator struct {
 	MapTaskList    []Task
 	ReduceTaskList []Task
 }
+type TaskStatus int
+
+const (
+	Idle TaskStatus = iota
+	InProgress
+	Complete
+)
 
 type Task struct {
-	Status     string
+	Status     TaskStatus
 	AssignedAt time.Time
 }
 
@@ -28,9 +35,9 @@ func (c *Coordinator) AssignTask(args *Args, reply *Reply) error {
 	if c.Mapping {
 		notComplete := false
 		for i, task := range c.MapTaskList {
-			if task.Status == "in-progress" {
+			if task.Status == InProgress {
 				if time.Since(task.AssignedAt) > 10*time.Second {
-					c.MapTaskList[i].Status = "idle"
+					c.MapTaskList[i].Status = Idle
 				} else {
 					notComplete = true
 				}
@@ -38,13 +45,13 @@ func (c *Coordinator) AssignTask(args *Args, reply *Reply) error {
 		}
 
 		for i, task := range c.MapTaskList {
-			if task.Status == "idle" {
+			if task.Status == Idle {
 				reply.TaskNo = i
 				reply.NReduce = c.NReduce
 				reply.FileName = c.Files[i]
 				reply.TaskType = "map"
 				c.MapTaskList[i].AssignedAt = time.Now()
-				c.MapTaskList[i].Status = "in-progress"
+				c.MapTaskList[i].Status = InProgress
 				return nil
 			}
 		}
@@ -60,9 +67,9 @@ func (c *Coordinator) AssignTask(args *Args, reply *Reply) error {
 	if !c.Complete {
 		notComplete := false
 		for i, task := range c.ReduceTaskList {
-			if task.Status == "in-progress" {
+			if task.Status == InProgress {
 				if time.Since(task.AssignedAt) > 10*time.Second {
-					c.ReduceTaskList[i].Status = "idle"
+					c.ReduceTaskList[i].Status = Idle
 				} else {
 					notComplete = true
 				}
@@ -70,12 +77,12 @@ func (c *Coordinator) AssignTask(args *Args, reply *Reply) error {
 		}
 
 		for i, task := range c.ReduceTaskList {
-			if task.Status == "idle" {
+			if task.Status == Idle {
 				reply.NMap = c.NMap
 				reply.TaskType = "reduce"
 				reply.TaskNo = i
 				c.ReduceTaskList[i].AssignedAt = time.Now()
-				c.ReduceTaskList[i].Status = "in-progress"
+				c.ReduceTaskList[i].Status = InProgress
 				return nil
 			}
 		}
@@ -93,9 +100,9 @@ func (c *Coordinator) AssignTask(args *Args, reply *Reply) error {
 
 func (c *Coordinator) ReportComplete(args *UpdateMessage, reply UpdateRecieved) error {
 	if args.TaskType == "map" {
-		c.MapTaskList[args.TaskNo].Status = "complete"
+		c.MapTaskList[args.TaskNo].Status = Complete
 	} else {
-		c.ReduceTaskList[args.TaskNo].Status = "complete"
+		c.ReduceTaskList[args.TaskNo].Status = Complete
 	}
 	reply.Acknowledge = true
 	return nil
@@ -127,14 +134,16 @@ func (c *Coordinator) Done() bool {
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
 func MakeCoordinator(sockname string, files []string, nReduce int) *Coordinator {
+	mapTaskList := make([]Task, len(files))
+	reduceTaskList := make([]Task, nReduce)
 	c := Coordinator{
-		NMap:               len(files),
-		NReduce:            nReduce,
-		CurrentFileIndex:   0,
-		CurrentBucketIndex: 0,
-		Files:              files,
-		Mapping:            true,
-		Reducing:           false,
+		NMap:           len(files),
+		NReduce:        nReduce,
+		Files:          files,
+		Mapping:        true,
+		Complete:       false,
+		MapTaskList:    mapTaskList,
+		ReduceTaskList: reduceTaskList,
 	}
 
 	c.server(sockname)
